00d144119724fee3208ec2de481c0d50bb2b9972
Adam Tharani <adamtharani@me.com>
2021-05-17 11:21:15 -0400

    Directory BS
    Signed-off-by: Adam Tharani <adamtharani@me.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,4 +48,4 @@ helm package .
 CHART_NAME=$(helm inspect chart . | tail -4 | head -n 1 | awk '{print $2}')
 CHART_VERSION=$(helm inspect chart . | tail -2 | tr -d "\n" | awk '{print $2}')
 
-helm push ${CHART_FOLDER}${CHART_NAME}-${CHART_VERSION}.tgz ${CHARTMUSEUM_URL} -u ${CHARTMUSEUM_USER} -p ${CHARTMUSEUM_PASSWORD} ${FORCE}
+helm push ${CHART_NAME}-${CHART_VERSION}.tgz ${CHARTMUSEUM_URL} -u ${CHARTMUSEUM_USER} -p ${CHARTMUSEUM_PASSWORD} ${FORCE}
